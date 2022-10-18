19a9eb1838f8fb5ecb5e8632bd3c88220852d291
linearhex <90122761+linearhex@users.noreply.github.com>
2022-10-18 16:27:47 +0800

    Update status.js

--- handler/status.js
+++ handler/status.js
@@ -1,7 +1,7 @@
 module.exports = (bot) => {
     bot.status(
-     { text: "/gowon", type: "LISTENING", status: "idle", time: 3 }, 
-     { text: "boost4perks", type: "LISTENING", status: "idle", time: 3 },
+     { text: "/zitergp", type: "LISTENING", status: "idle", time: 3 }, 
+     { text: "Loveable", type: "LISTENING", status: "idle", time: 3 },
      { text: "<3", type: "PLAYING", status: "idle", time: 3 }
   )
 }
